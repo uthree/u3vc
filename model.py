1ee84827d58b360ae460d65b363a430b38055694
uthree <uthree271828@gmail.com>
2023-05-15 04:11:06 +0900

    fix

--- model.py
+++ model.py
@@ -95,7 +95,7 @@ class GeneratorResStack(nn.Module):
     def __init__(self,
             channels,
             condition_channels=128,
-            num_layers=4):
+            num_layers=2):
         super().__init__()
         self.layers = nn.ModuleList([])
         for _ in range(num_layers):
